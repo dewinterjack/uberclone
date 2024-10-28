1f46c02385b04d6da4581c0cad56fb8b9fca69b0
Jack Dewinter <jackdewinter1@gmail.com>
2024-10-28 19:02:26 +0100

    passing local build

---
+++ apps/riders/next.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from "url";
+import { createJiti } from "jiti";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@acme/api", "@acme/db"],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  
+};
+
+export default config;
